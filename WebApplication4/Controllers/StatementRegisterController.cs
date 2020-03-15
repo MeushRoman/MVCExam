abba9ca678689d6f28217d7db65750a38560237c
Roman Dark <amaii.sakura@gmail.com>
2020-03-16 00:15:41 +0600

    addSteage

---
+++ WebApplication4/Controllers/StatementRegisterController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class StatementRegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowModal()
+        {
+            var documents = new List<Document>()
+            {
+                new Document{Id = 1, Name = "stage1", Data="<div><h1>It's TeSt1</h1></div>"},
+                new Document{Id = 2, Name = "stage2", Data="<div><h1>It's TeSt2</h1></div>"},
+                new Document{Id = 3, Name = "stage3", Data="<div><h1>It's TeSt3</h1></div>"}
+            };
+            return View(documents);
+        }
+        public bool ShowModal1(Document response)
+        {
+           
+            return true;
+        }
+
+        [HttpPost]
+        public IActionResult Index(Document response)
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ WebApplication4/wwwroot/js/UniversalModalForm.js
@@ -0,0 +1,55 @@
+﻿'use strict'
+
+var Modal = {};
+
+Modal.Controller = "";
+Modal.Action = "";
+
+Modal.Get = (controller, action, size, param) => {
+    debugger
+    Modal.Controller = controller;
+    Modal.Action = action;
+
+    $.ajax({
+        type: "GET",
+        url: `/${controller}/${action}/${param}`,
+        success: (response) => {
+            $("#ModalDialogSizeId").addClass(`modal-${size}`);
+            $("#BodyModal").empty();
+            $("#BodyModal").append(response);
+            $("#UniversalModalId").modal('show');
+        }
+    })
+};
+
+Modal.Post = () => {
+    var form = new FormData(document.forms["UniversalModalForm"]);
+    $.ajax({
+        type: "POST",
+        url: `/${Modal.Controller}/${Modal.Action}`,
+        data: form,
+        dataType: "json",
+        contentType: false,
+        processData: false,
+        success: (response) => {
+            $("#BodyModal").empty();
+            $("#BodyModal").append(response);
+            $("#UniversalModalId").modal("show");
+        }
+    })
+};
+
+Modal.Post2 = (controller, action) => {
+    debugger
+    var form = new FormData(document.forms["UniversalModalForm"]);
+    $.ajax({
+        type: "POST",
+        url: `/${controller}/${action}`,
+        data: form,
+        dataType: "json",
+        contentType: false,
+        processData: false,
+        success: (response) => {          
+        }
+    })
+};
